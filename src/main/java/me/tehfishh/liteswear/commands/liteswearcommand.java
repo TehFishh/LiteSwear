ae1e6864d10a5d6e6a124be0186674300bd967fc
fionster_fish_1 <fiontan@gmx.co.uk>
2016-12-24 10:09:35 +0000

    main config add elements complete
    Experimental build release

--- src/main/java/me/tehfishh/liteswear/commands/liteswearcommand.java
+++ src/main/java/me/tehfishh/liteswear/commands/liteswearcommand.java
@@ -6,6 +6,8 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import java.util.List;
+
 public class liteswearcommand implements CommandExecutor {
 
     public static liteswear plugin;
@@ -20,12 +22,28 @@ public class liteswearcommand implements CommandExecutor {
             if (sender.hasPermission("liteswear.admin")) {
                 if (args.length == 0) {
                     sender.sendMessage(ChatColor.translateAlternateColorCodes('&', invalidarguement()));
+                } else if (args.length >= 2) {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', invalidarguement()));
                 } else {
                     if (args[0].equalsIgnoreCase("reload")) {
                         plugin.reloadConfig();
                         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&lLiteswear &8> &7Reloaded config successfully."));
                     } else if (args[0].equalsIgnoreCase("help")) {
-                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&lLiteswear &8> &7Valid arguements: Reload"));
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&lLiteswear &8> &7Valid arguements: reload, addword"));
+                    } else if (args[0].equalsIgnoreCase("addword")) {
+                        if (args.length < 2) {
+                            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&lLiteswear &8> &cCorrect syntax: /liteswear addword (word)"));
+                        } else {
+                            if (args.length == 2) {
+                                String arg2 = args[1];
+                                List<String> blockedwords = plugin.getConfig().getStringList("Blocked Words");
+                                blockedwords.add(arg2);
+                                plugin.getConfig().set("Blocked Words", blockedwords);
+                                plugin.saveConfig();
+                                plugin.reloadConfig();
+                                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9&lLiteswear &8> &7Added &6" + arg2 + " &7blocked word list"));
+                            }
+                        }
                     }
                 }
             } else {
